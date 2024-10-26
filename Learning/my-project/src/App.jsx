550701ad945cee0f04b130ccf9ade4a0363685ed
Nycht <petersoncararajunior@gmail.com>
2024-10-26 04:08:12 -0300

    Reducing Items

--- Learning/my-project/src/App.jsx
+++ Learning/my-project/src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
-import { Login } from './pages/auth/login'
-import { Register } from './pages/auth/register'
 import { LandingPage } from './pages/main/landingpage'
 
 function App() {
   return (
     <>
-      {/*<Register/>
-        <Login/>*/}
       <LandingPage/>
     </>
   )
--- Learning/my-project/src/pages/main/landingpage.jsx
+++ Learning/my-project/src/pages/main/landingpage.jsx
@@ -1,13 +1,9 @@
-import { Aside } from '../../stuff/page_stuff/aside'
-import { Hero } from '../../stuff/page_stuff/main'
 import '../../App.css'
 
 export function LandingPage() {
     return(
         <>
             <main class='main'>
-                <Aside/>
-                <Hero/>
             </main>
         </>
     )
